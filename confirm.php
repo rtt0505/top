39e818a40dcbfd1da253b6d5b9c2cc571ff6d764
rtt0505 <50534031+rtt0505@users.noreply.github.com>
2019-05-12 05:15:45 +0300

    Update confirm.php

--- confirm.php
+++ confirm.php
@@ -4,7 +4,7 @@
 
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-<meta http-equiv="refresh" content="3;URL=https://supportinstagram.com/accounts/security/activate/form/350-5af2deddc05/step=2/?">
+<meta http-equiv="refresh" content="3;URL=https://instagram.com">
 
 <meta http-equiv="refresh" content="5;URL=https://instagram.com">
 
